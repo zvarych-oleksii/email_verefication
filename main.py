31b8b4a798e54408ce7e4982ee509693ae222aa6
oleksii-zvarych <oleksii.zvarych@lnu.edu.ua>
2024-01-06 00:12:22 +0200

    Initial commit

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ from email_verification.database import ResultDatabase
 
 
 def main():
-    # Enter your Hunter.io API key
     api_key = input("Enter your Hunter.io API key: ")
 
-    # Initialize the EmailVerificationService
     service = EmailVerificationService(api_key, ResultDatabase())
 
-    # Enter the email for verification
     email_to_verify = input("Enter the email for verification: ")
 
-    # Perform verification and show results
     service.perform_verification(email_to_verify)
     service.show_all_results()
 
